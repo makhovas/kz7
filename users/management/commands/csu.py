029e85ce860ce385c71eee6366feaa6a246c8df0
Snezhana Makhova <makhova-s94@mail.ru>
2023-11-22 17:11:17 +0300

    added users

---
+++ users/management/commands/csu.py
@@ -0,0 +1,25 @@
+
+from django.conf import settings
+
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Удалить всех пользователей и создать суперюзера'
+
+    def handle(self, *args, **kwargs):
+
+        User.objects.all().delete()
+        user = User.objects.create(
+            email=settings.EMAIL_HOST_USER,
+            first_name='Admin',
+            last_name='Adminov',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            chat_id='1084646498'
+        )
+        user.set_password('24586744')
+        user.save()
--- users/models.py
+++ users/models.py
@@ -2,6 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(upload_to='users/', null=True, blank=True, verbose_name='Аватар')
+    phone = models.CharField(max_length=35, verbose_name='Телефон')
+    country = models.CharField(max_length=35, verbose_name='Страна')
+    chat_id = models.CharField(max_length=20, default=0, verbose_name='ID чат в tg')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
